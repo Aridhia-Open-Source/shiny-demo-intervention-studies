b7c8cc66239eaf0c276fc235f47d70bbc358c9f2
Harry Peaker <HarryPeaker@Aridhia.net>
2021-01-14 10:22:38 +0000

    deploy to shinyapps.io

--- global.R
+++ global.R
@@ -5,8 +5,10 @@ library(shinydashboard)
 library(shinythemes)
 library(dplyr)
 
-source("scorer.R")
-
 trusts <- read.csv("data/trusts.csv")
 recalibrated <- readRDS("data/recalibrated_model.rds")
 parr30_sample <- read.csv("data/parr30_sample.csv")
+
+source("scorer.R")
+
+
