f2ecb9ccd6c51ed8bf6b1ae311a4a930991e3341
annadomenech <anna.domenech@aridhia.com>
2021-01-28 11:24:24 +0000

    added video

--- code/help_tab.R
+++ code/help_tab.R
@@ -4,13 +4,14 @@ documentation_tab <- function() {
                      fluidRow(column(
                        6,
                        h3("Intervention Studies"), 
-                       p("TThis RShiny mini-app is a visualisation tool that shows the predicted classification of 200 patients from a synthetic sample dataset created 
-                         by Aridhia.Using this sample data, we calculated the predicted risk of readmission within 30 days using the default original PARR-30 coefficients. 
+                       p("This RShiny mini-app is a visualisation tool that shows the predicted classification of 200 patients from a synthetic sample dataset created 
+                         by Aridhia. Using this sample data, we calculated the predicted risk of readmission within 30 days using the PARR-30 model. 
                          As the real outcome of each of these patients is known, it allows for comparison with the predicted model."),
+                       p("It also allows the user to calculate the readmission risk of a patient according to certain characteristics."),
                        h5("How use the mini-app"),
                        p("In the first tab, you can edit the thresholds for each risk group and select the threshold to decide which cases should be intervened. 
                          Then, you can press play to start the simulation." ),
-                       p("In the second tab, ")
+                       p("In the second tab, you can fill the inputs based on a patient's characteristics. When you are done, click on calculate risk.")
                      ),
                      column(
                        6,
